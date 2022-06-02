ff1006c344da521ed272bf72385c6b22db20233e
Sebastian Denis <sebastian.denis@outlook.com>
2022-06-02 14:10:23 +0200

    lesson 102

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -1,10 +1,13 @@
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { Fragment } from 'react';
 import fs from 'fs/promises';
 import path from 'path';
 
 const ProductDetailPage = (props: any) => {
   const { loadedProduct } = props;
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -13,13 +16,17 @@ const ProductDetailPage = (props: any) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async (context: any) => {
-  const { params } = context;
-  const productId = params.pid;
-
+export const getData: any = async (context: any) => {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath, 'utf-8');
   const data = JSON.parse(jsonData);
+  return data;
+};
+
+export const getStaticProps: GetStaticProps = async (context: any) => {
+  const { params } = context;
+  const productId = params.pid;
+  const data = await getData();
 
   const product = data.products.find(
     (product: any) => product.id === productId
@@ -29,6 +36,16 @@ export const getStaticProps: GetStaticProps = async (context: any) => {
     props: {
       loadedProduct: product
     }
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async (context: any) => {
+  const data = await getData();
+  const ids = data.products.map((product: any) => product.id);
+  const pathsWithParams = ids.map((id: string) => ({params: {pid: id}}));
+  return {
+    paths: pathsWithParams,
+    fallback: false // it can be false or blocking also
   };
 };
 
